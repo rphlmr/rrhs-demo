f7a1a9ff1d658dc0ac8c079587680ffe27cdbfc6
Herc <hi@herkulano.com>
2025-08-01 16:50:18 +0300

    test: use getContext() with AsyncLocalStorage

--- app/components/location.tsx
+++ app/components/location.tsx
@@ -3,5 +3,5 @@ import React from "react";
 export default function Location(props: { location: Promise<string> }) {
 	const data = React.use(props.location);
 
-	return <p>Your project is at {data}.</p>;
+	return <p>Location: {data}</p>;
 }
--- app/routes/defer.tsx
+++ app/routes/defer.tsx
@@ -3,20 +3,23 @@ import type { Route } from "./+types/defer";
 import Location from "~/components/location";
 import { Link } from "react-router";
 import { getLocation } from "~/data/get-location";
+import { getContext } from "~/server/middleware/context";
 
 export async function loader() {
 	return {
-		location: getLocation("DEFERRED WORKS!"),
+		location: getLocation("WORKS!"),
+		ctx: getContext(),
 	};
 }
 
 export default function ProjectRoute({ loaderData }: Route.ComponentProps) {
 	return (
 		<main>
-			<h1>Let's locate your project</h1>
-			<React.Suspense fallback={<p>Loading project location...</p>}>
+			<h1>Check defer</h1>
+			<React.Suspense fallback={<p>Loading location...</p>}>
 				<Location location={loaderData.location} />
 			</React.Suspense>
+			<p className="text-xs text-gray-500">{JSON.stringify(loaderData.ctx)}</p>
 			<Link to="/">Go to home →</Link>
 		</main>
 	);
