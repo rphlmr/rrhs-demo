f2f587e27d1d4546290b6b33530e9d459af4be6e
rphlmr <raphael@uaerom.com>
2025-08-01 14:14:59 +0200

    init

--- vite.config.bun.ts
+++ vite.config.bun.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    reactRouterHonoServer(),
+    reactRouterHonoServer({ runtime: "bun" }),
     tailwindcss(),
     reactRouter(),
     tsconfigPaths(),
